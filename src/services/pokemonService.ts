1e7ce919f719cef9a4b2c30b4a807fcd22c70e84
Theosaurus-Rex <theosaurus13@gmail.com>
2025-12-14 10:10:11 +1100

    feat: Add function to fetch list of pokemon for the grid

--- src/services/pokemonService.ts
+++ src/services/pokemonService.ts
@@ -30,3 +30,27 @@ export const fetchPokemon = async (id: number | string): Promise<Pokemon> => {
   pokemonCache[id] = pokemon;
   return pokemon;
 };
+
+type PokemonListResults = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Array<{ name: string; url: string }>;
+};
+
+export const fetchPokemonList = async (
+  offset: number,
+  limit: number,
+): Promise<PokemonListResults> => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`,
+  );
+  const pokemonList = await response.json();
+
+  return {
+    next: pokemonList.next,
+    previous: pokemonList.previous,
+    results: pokemonList.results,
+    count: pokemonList.count,
+  };
+};
